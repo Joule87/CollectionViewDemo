bd734be403d7909ffc7f71cd0f722c757ee4f823
Julio Collado <jcollado@altimetrik.com>
2020-04-17 18:15:28 -0300

    Customize Section Header

---
+++ WiredBrainCoffee/Scenes/HomeViewController/HeaderCollectionReusableView.swift
@@ -0,0 +1,15 @@
+//
+//  HeaderCollectionReusableView.swift
+//  WiredBrainCoffee
+//
+//  Created by Julio Collado on 4/17/20.
+//  Copyright © 2020 Mark Moeykens. All rights reserved.
+//
+
+import UIKit
+
+class HeaderCollectionReusableView: UICollectionReusableView {
+        
+    @IBOutlet weak var headerLabel: UILabel!
+    
+}
